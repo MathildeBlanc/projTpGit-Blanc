df38baf1f4f8f6c051e03d3e42be426fdab41ef1
Unknow <>
2021-10-22 09:05:21 +0200

    blob

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo 'bonjour de Blanc';
+        echo 'bonjour de Blanc voici son numéro de téléphne ';
         ?>
     </body>
 </html>
