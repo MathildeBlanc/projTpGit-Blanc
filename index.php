4b6b4af5cc861a910f92f24b08ce67db26ba9413
test <test@debian>
2021-10-22 09:09:39 +0200

    blob correction

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo 'bonjour de Blanc voici son numéro de téléphne ';
+        echo 'bonjour de Blanc voici son numéro de téléphone ';
         ?>
     </body>
 </html>
