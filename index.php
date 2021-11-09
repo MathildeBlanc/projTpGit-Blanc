8a91f142f404c6d5bbc04dc961a19cc383ce7fe3
mathildeblanc.bts20@gmail.com <test@debian>
2021-11-09 11:01:41 +0100

    Ajout saut de ligne

--- index.php
+++ index.php
@@ -12,8 +12,9 @@ and open the template in the editor.
     <body>
         <?php
         require 'fichier1-Blanc.php';
-        echo 'Bonjour de Blanc';
-        echo 'seconde ligne de programme pour utilisateur Blanc';
+        echo 'Bonjour de Blanc'; 
+        echo '<br>';
+        echo 'Seconde ligne de programme pour utilisateur Blanc';
         ?>
     </body>
 </html>
